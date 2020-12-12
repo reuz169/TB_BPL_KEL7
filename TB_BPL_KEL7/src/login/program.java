a2f6e2cf88d311aa04723f68401c28799ab74a9e
idris <Abdi@DESKTOP-2110DRS>
2020-12-12 09:33:37 +0700

    percobaan pertama menggunakan github

---
+++ TB_BPL_KEL7/src/login/program.java
@@ -0,0 +1,11 @@
+package login;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("bismillah ya allah..... yok bisa yok");
+
+	}
+
+}
